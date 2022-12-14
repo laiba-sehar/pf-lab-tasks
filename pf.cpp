fa68235f9d90917ef82da2dfe4450e3dc07eb2bd
Hair PC <seharlaiba772@gmail.com>
2022-12-14 17:19:17 +0000

    mrssage

---
+++ pf.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+main()
+{
+ cout<<"Hello world"<<endl<<endl;
+ cout<<"here this is the output";
+}
---
+++ task6.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+main()
+{
+cout<<"        ---------"<<endl;
+cout<<"        ||o  o ||"<<endl;
+cout<<"        ||  -  ||"<<endl;
+cout<<"        ||  v  ||"<<endl;
+cout<<"         -------"<<endl;
+cout<<"          || || "<<endl;
+cout<<"        ----------"<<endl;
+cout<<"     // ||      ||\\"<<endl;
+cout<<"    //  ||      || \\"<<endl;
+cout<<"   //   ||      ||  \\"<<endl;
+cout<<"  O     ||      ||   O"<<endl;
+cout<<"        ----------"<<endl;
+cout<<"          ||  ||"<<endl;
+cout<<"          ||  ||"<<endl;
+cout<<"           O  O"<<endl;
+}
